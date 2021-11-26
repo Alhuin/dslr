e06670c9aedfd96be8947f755d68db8cf18570fe
Alhuin <jjanin-r@student.42lyon.fr>
2021-11-26 16:39:51 +0100

    assert output and not return in test_describe_on_test_dataset naming

--- tests/test_describe.py
+++ tests/test_describe.py
@@ -4,6 +4,8 @@ from constants import test_dataset_path
 from dslr.describe import describe
 
 
-def test_main_output_w_test_dataset(capfd):
+def test_describe_on_test_dataset(capfd):
     csvfile = pandas.read_csv(test_dataset_path, encoding='utf-8')
-    assert describe(test_dataset_path) == csvfile.describe().__str__()
+    describe(test_dataset_path)
+    out, err = capfd.readouterr()
+    assert out == csvfile.describe().__str__()
