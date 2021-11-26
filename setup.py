d98f43f806b7ab2d721853ed9aa7ee1637ecd401
Alhuin <jjanin-r@student.42lyon.fr>
2021-11-26 17:00:14 +0100

    pre-commit flake8, end of file fixer, trailing whitespace

--- setup.py
+++ setup.py
@@ -1,23 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='dslr',
-    version='1.0.0',
-    description='42 AI : Logistic Regression Project',
-    author='jjanin-r',
-    author_email='jjanin-r@student.42lyon.fr',
-    packages=['dslr'],
-    extras_require={
-        'dev': [
-            'pytest',
-            'flake8',
-            'pytest-cov',
-            'pandas'
-        ],
-    },
-    install_requires=[
-        'wheel',
-        'matplotlib',
-        'numpy'
-    ],
+    name="dslr",
+    version="1.0.0",
+    description="42 AI : Logistic Regression Project",
+    author="jjanin-r",
+    author_email="jjanin-r@student.42lyon.fr",
+    packages=["dslr"],
+    extras_require={"dev": ["pytest", "flake8", "pytest-cov", "pandas", "pre-commit"]},
+    install_requires=["wheel", "matplotlib", "numpy"],
 )
